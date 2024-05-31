170f6daf8e969de9a63e88be5642b1e2fd5bff94
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-05-31 14:07:46 +0200

    Remove unwanted console.log

--- app/error.js
+++ app/error.js
@@ -2,6 +2,5 @@
 
 export default function RootError() {
   // You can add any UI inside RooError, including Animations.
-  console.log('Error  page');
   return <div>Ups! something went wrong</div>;
 }

2a5886a2af12756aad6afcface5c5a6223cae91d
Karl Horky <karl.horky@gmail.com>
2024-08-26 11:52:39 +0200

    Switch globalThis var to local let var in IIFE closure

--- database/connect.ts
+++ database/connect.ts
@@ -6,37 +6,26 @@ import { setEnvironmentVariables } from '../util/config';
 
 setEnvironmentVariables();
 
-declare module globalThis {
-  let postgresSqlClient: Sql;
-}
-
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
-function connectOneTimeToDatabase() {
-  if (!('postgresSqlClient' in globalThis)) {
-    globalThis.postgresSqlClient = postgres(postgresConfig);
-  }
+const connectOneTimeToDatabase = (() => {
+  let postgresSqlClient: Sql;
 
-  // Workaround to force Next.js Dynamic Rendering on every database query:
-  //
-  // Wrap sql`` tagged template function to call `noStore()` from
-  // next/cache before each database query. `noStore()` is a
-  // Next.js Dynamic Function, which causes the page to use
-  // Dynamic Rendering
-  //
-  // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering
-  //
-  // Ideally there would something built into Next.js for this,
-  // which has been requested here:
-  //
-  // https://github.com/vercel/next.js/discussions/50695
-  return ((
-    ...sqlParameters: Parameters<typeof globalThis.postgresSqlClient>
-  ) => {
-    noStore();
-    return globalThis.postgresSqlClient(...sqlParameters);
-  }) as typeof globalThis.postgresSqlClient;
-}
+  return function connectOneTimeToDatabase() {
+    if (!postgresSqlClient) {
+      postgresSqlClient = postgres(postgresConfig);
+    }
+
+    // Workaround to force Next.js Dynamic Rendering on every database query
+    // https://github.com/vercel/next.js/discussions/50695
+    return ((
+      ...sqlParameters: Parameters<typeof postgresSqlClient>
+    ) => {
+      noStore();
+      return postgresSqlClient(...sqlParameters);
+    }) as typeof postgresSqlClient;
+  };
+})();
 
 export const sql = connectOneTimeToDatabase();
 

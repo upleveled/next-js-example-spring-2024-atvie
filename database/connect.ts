1fef1ceee60f671a4631d1c32807710c98c857ed
Victor Ejike Nwosu <74430629+Eprince-hub@users.noreply.github.com>
2024-09-04 17:56:18 +0200

    Add new dynamicIO flag and remove unstable_noStore (#10)
    * Update project
    
    * Update database/connect.ts
    
    ---------
    
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- database/connect.ts
+++ database/connect.ts
@@ -1,5 +1,4 @@
 import 'server-only';
-import { unstable_noStore as noStore } from 'next/cache';
 import postgres, { Sql } from 'postgres';
 import postgresConfig from '../ley.config';
 import { setEnvironmentVariables } from '../util/config';
@@ -17,37 +16,8 @@ function connectOneTimeToDatabase() {
     globalThis.postgresSqlClient = postgres(postgresConfig);
   }
 
-  // Workaround to force Next.js Dynamic Rendering on every database query:
-  //
-  // Wrap sql`` tagged template function to call `noStore()` from
-  // next/cache before each database query. `noStore()` is a
-  // Next.js Dynamic Function, which causes the page to use
-  // Dynamic Rendering
-  //
-  // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering
-  //
-  // Ideally there would something built into Next.js for this,
-  // which has been requested here:
-  //
-  // https://github.com/vercel/next.js/discussions/50695
-  return ((
-    ...sqlParameters: Parameters<typeof globalThis.postgresSqlClient>
-  ) => {
-    noStore();
-    return globalThis.postgresSqlClient(...sqlParameters);
-  }) as typeof globalThis.postgresSqlClient;
+  return globalThis.postgresSqlClient;
 }
 
+// Connect to PostgreSQL
 export const sql = connectOneTimeToDatabase();
-
-// import { config } from 'dotenv-safe';
-// import postgres from 'postgres';
-
-// config();
-
-// export const sql = postgres({
-// transform: {
-//   ...postgres.camel,
-//   undefined: null,
-// },
-// });
